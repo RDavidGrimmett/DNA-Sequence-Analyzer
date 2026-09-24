b3a0169b123f249321a334b4182f800d9b083153
David Grimmett <r.david.grimmett@gmail.com>
2026-09-24 17:44:51 +0000

    Created dna_pipeline.py and mock sequences.fasta

---
+++ dna_pipeline.py
@@ -0,0 +1,13 @@
+#this is dna_pipeline.py
+#first Python-based bioinformatics pipeline by using reusable functions and Python libraries like pandas or NumPy.
+
+import pandas 
+import NumPy
+
+def data_loading():
+
+def sequence_filtering():
+
+def analysis():
+
+def summary_and_saving_results():
